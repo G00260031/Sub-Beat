96ca682c3e2bc9d002d4c43293fa3e650a14df35
Giovanny Ospina <31867636+ospna@users.noreply.github.com>
2019-02-21 11:53:53 -0500

    Subbeat

---
+++ Assets/Scripts/Actions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Actions : MonoBehaviour
+{
+    public void GameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    /*
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void UpgradeMenu()
+    {
+        SceneManager.LoadScene("UpgradeMenu");
+    }
+
+    public void Music()
+    {
+        SceneManager.LoadScene("Music");
+    }
+    */
+
+}
